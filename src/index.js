f910396d5fd29c7e1231c1647787a6904ab6bb71
KristinaShepeleva <shepelevakristina52@gmail.com>
2023-03-22 22:52:40 +0200

    2

--- src/index.js
+++ src/index.js
@@ -1,72 +1,83 @@
-
-
-const formRef = document.querySelector(".search-form");
-const inputRef = formRef.querySelector("input");
-const buttonRef = formRef.querySelector("button");
-const divRef = document.querySelector('gallery')
-
-console.log(formRef);
-
-//inputRef.addEventListener('input', onInput);
-formRef.addEventListener('submit', onSearchBtn);
+import { fetchImg } from './js/fetchImages';
+import Notiflix from 'notiflix';
 
 
 
-async function onSearchBtn(e) {
-    e.preventDefault();
-
-    let searchValue = inputRef.value.trim();
-
-    console.log(searchValue);
+const refs = {
+  formSearch: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  //inputRef:  formSearch.querySelector("input"),
+  //buttonRef: formSearch.querySelector("button"),
 };
 
 
-// function onSearchBtn(event) {
-//     event.preventDefault();
-//      try {
-//     const imgs = await fetchImgs();
-//     createMarcup(imgs);
-//   }  catch (error) {
-//     console.log(error.message);
-//   }
-// };
+//inputRef.addEventListener('input', onInput);
+refs.formSearch.addEventListener('submit', onSearchBtn);
+
+async function onSearchBtn(e) {
+  e.preventDefault();
+
+  let searchValue = e.currentTarget.elements.searchQuery.value.trim();
+
+   //console.log(inputRef.value);
+  //console.log(e.currentTarget.elements.searchQuery.value);
+  
+  if (searchValue) {
+    const images = await fetchImg(searchValue);
+    if (images.length > 0) {
+      refs.formSearch.reset();
+      clearAll()
+      Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
+      console.log(images);
+    } else
+      
+      
+      refs.formSearch.reset();
+      
+      createImage(images);
+      Notiflix.Notify.success(`Hooray! We found ${images.totalHits} images.`);
+
+    
+
+    } 
+  }
 
 
-
-
-
-
-
-function createMarcup(imgs) {
-    clearMarkup();
-    const markupList = imgs
-     .map(img => {
-       return `<div class="photo-card">
-<a class="img-big" href="${img.largeImageURL}">
-  <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
-</a>
-  <div class="info">
-    <p class="info-item">${img.likes}
-      <b>Likes</b>
-    </p>
-    <p class="info-item">${img.views}
-      <b>Views</b>
-    </p>
-    <p class="info-item">${img.comments}
-      <b>Comments</b>
-    </p>
-    <p class="info-item">${img.downloads}
-      <b>Downloads</b>
-    </p>
-  </div>
-</div>`;
-     }).join("");
-    divRef.innerHTML = markupList;
+function createImage(data) {
+  let markupPost = data
+    .map(
+      ({
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<a class="gallery__item" href="${largeImageURL}">
+                  <div class="photo-card">
+                      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+                      <div class="info">
+                        <p class="info-item"><b>Likes</b> ${likes}</p>
+                        <p class="info-item"><b>Views</b> ${views}</p>
+                        <p class="info-item"><b>Comments</b> ${comments}</p>
+                        <p class="info-item"><b>Downloads</b> ${downloads}</p>
+                      </div>
+                    </div>
+                 </a>`;
+      }
+    )
+    .join('');
+   
+  refs.gallery.insertAdjacentHTML('beforeend', markupPost);
+  //lightbox.refresh();
+  // smoothScroll();
 }
 
 
- function clearMarkup() {
-   divRef.innerHTML = '';
- };
+function clearAll() {
+    refs.gallery.innerHTML = '';
+};
 
-
+ 
---
+++ src/js/fetchImages.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export const DEFAULT_PAGE = 1;
+export let page = DEFAULT_PAGE;
+
+export async function fetchImg(searchValue) {
+    const searchParams = new URLSearchParams({
+        key: '10836129-2832e3ff848e050d6947a018c',
+        q: searchValue,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        per_page: 40,
+        page
+    });
+     
+    try {
+        const images = await axios.get(`https://pixabay.com/api/?${searchParams}`).then(page += 1);
+        //console.log(images);
+        //console.log(images.data);
+        //console.log(images.data.hits);
+        return images.data.hits;
+    } catch (error) {
+    console.error(error);
+   }
+}
+
+export function resetPage() {
+    page = DEFAULT_PAGE;
+};
+
