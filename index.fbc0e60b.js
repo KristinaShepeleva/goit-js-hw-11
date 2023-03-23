a979277ed9adaf2475284d75552f0d2d7789485c
KristinaShepeleva <113527872+KristinaShepeleva@users.noreply.github.com>
2023-03-23 17:44:23 +0000

    Deploying to gh-pages from @ KristinaShepeleva/goit-js-hw-11@f910396d5fd29c7e1231c1647787a6904ab6bb71 🚀

--- index.fbc0e60b.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".search-form"),t=e.querySelector("input");e.querySelector("button"),document.querySelector("gallery");console.log(e),e.addEventListener("submit",(async function(e){e.preventDefault();let o=t.value.trim();console.log(o)}));
-//# sourceMappingURL=index.fbc0e60b.js.map
